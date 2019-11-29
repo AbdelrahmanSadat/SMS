47fe20c665dddddfae26faa86415a1d1d51898a0
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2019-11-29 15:00:17 +0200

    - fix: minor fix for db path - was creating the database in upper directories - now creates db in the app's root dir.

--- app/utils/database/index.js
+++ app/utils/database/index.js
@@ -1,6 +1,10 @@
 //* Setting up the database with the default
 //* path we're using.
 let sequelizeConfig = require("./db");
-let models = sequelizeConfig("../../../dbtest.db")
+let models = sequelizeConfig("dbtest.db")
+console.log("THE DB PATH")
+console.log(__dirname)
+
+// ? This creates the DB in /electron-react (app's root directory?)
 
 module.exports = models;
