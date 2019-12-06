5910022287f0644e3d991a94fe1f0909fa00a8dd
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2019-12-06 14:59:45 +0200

    - feat: implemented the functionality of admission - modified admission component to work nicely with added functionality - also modified admission component a bit to add some missing inputs - modified admissionPage to have all the needed logic for admission - TODO: admissoinPage needs some refactoring

--- app/components/Admission/Admission.js
+++ app/components/Admission/Admission.js
@@ -1,59 +1,136 @@
-import React from 'react'
-import { Button, Form } from 'semantic-ui-react'
+import React from 'react';
+import { Button, Form } from 'semantic-ui-react';
 
-// const options = [
-//   { key: 'm', text: 'Male', value: 'male' },
-//   { key: 'f', text: 'Female', value: 'female' },
-//   { key: 'o', text: 'Other', value: 'other' },
-// ]
 
-const Admission = () => (
-  <Form>
-    <Form.Group unstackable widths={2}>
-      <Form.Input label='ID' placeholder='Increment last ID in DB' disabled />
-      <Form.Input label='Name' placeholder='Name' required />
+
+const Admission = props => (
+  <Form onSubmit={props.onSubmit}>
+    <Form.Group unstackable widths={3}>
+      <Form.Input
+        label="ID"
+        placeholder="Increment last ID in DB"
+        disabled
+        name="nextID"
+        value={props.admissionData.nextStudentID}
+        onChange={props.inputHandler}
+      />
+      <Form.Input
+        label="Name"
+        placeholder="Name"
+        required
+        name="name"
+        value={props.admissionData.name}
+        onChange={props.inputHandler}
+      />
+      <Form.Input
+        label="Email"
+        placeholder="Email"
+        required
+        name="email"
+        value={props.admissionData.email}
+        onChange={props.inputHandler}
+      />
     </Form.Group>
     <Form.Group widths={2}>
-      <Form.Input label='Phone Number' placeholder='Phone Number' required />
-      <Form.Input label='School' placeholder='School' />
+      <Form.Input
+        label="Phone Number"
+        placeholder="Phone Number"
+        required
+        name="phoneNumber"
+        value={props.admissionData.phoneNumber}
+        onChange={props.inputHandler}
+      />
+      <Form.Input
+        label="School"
+        placeholder="School"
+        name="school"
+        value={props.admissionData.school}
+        onChange={props.inputHandler}
+      />
+      <Form.Input
+        label="Address"
+        placeholder="Address"
+        name="address"
+        value={props.admissionData.address}
+        onChange={props.inputHandler}
+      />
+    </Form.Group>
+    <Form.Group widths={3}>
+      <Form.Input
+        label="Parent Name"
+        placeholder="Parent Name"
+        name="parentName"
+        value={props.admissionData.parentName}
+        onChange={props.inputHandler}
+      />
+      <Form.Input
+        label="Parent Occupation"
+        placeholder="Parent Occupation"
+        name="parentOccupation"
+        value={props.admissionData.parentOccupation}
+        onChange={props.inputHandler}
+      />
+      <Form.Input
+        label="Parent Phone Number"
+        placeholder="Parent Phone Number"
+        name="parentPhoneNumber"
+        value={props.admissionData.parentPhoneNumber}
+        onChange={props.inputHandler}
+      />
     </Form.Group>
     <Form.Group widths={2}>
-      <Form.Input label='Parent Occupation' placeholder='Parent Occupation' />
-      <Form.Input label='Parent Phone Number' placeholder='Parent Phone Number' />
-    </Form.Group>
-    <Form.Group widths={2}>
-    <Form.Select
-      required
-      fluid
-      label='Class'
-      // options={props.classes}
-      placeholder='Class'
+      <Form.Select
+        required
+        fluid
+        label="Class"
+        options={props.classOptions}
+        placeholder="Class"
+        name="class"
+        value={props.admissionData.class}
+        onChange={props.classInputHandler}
       />
       <Form.Select
         required
         fluid
-        label='Section'
-        // options={props.classes.sections}
-        placeholder='Section'
-        />
+        label="Section"
+        options={props.sectionOptions}
+        placeholder="Section"
+        name="section"
+        value={props.admissionData.section}
+        onChange={props.sectionInputHandler}
+      />
     </Form.Group>
     <Form.Group widths={2}>
-      <Form.Input label='Payment' value='Default Section Payment' required />
-      <Form.TextArea label='Notes' placeholder='Notes' />
+      {/* TODO: name this? */}
+      <Form.Input
+        label="Payment"
+        value="Default Section Payment"
+        required
+        name="payment"
+        value={props.admissionData.payment}
+        onChange={props.inputHandler}
+      />
+      <Form.TextArea
+        label="Notes"
+        placeholder="Notes"
+        name="notes"
+        value={props.admissionData.notes}
+        onChange={props.inputHandler}
+      />
     </Form.Group>
-    <Button type='submit'>Submit</Button>
-        <ul style={{ listStyleType: 'none' }}>
-          <li>Handling ID incrementation</li>
-          <li>
-            Getting the Class state to affect the Section state and hence the
-            default payment
-          </li>
-          <li>
-            On Submit: Create Student, Add Fees based on class and section, Set
-            Default Student Parameters
-          </li>
-        </ul>
+    <Button type="submit">Submit</Button>
+    <ul style={{ listStyleType: 'none' }}>
+      <li>Handling ID incrementation</li>
+      <li>
+        Getting the Class state to affect the Section state and hence the
+        default payment
+      </li>
+      <li>
+        On Submit: Create Student, Add Fees based on class and section, Set
+        Default Student Parameters
+      </li>
+    </ul>
   </Form>
-)
+);
 
-export default Admission
+export default Admission;
--- app/containers/AdmissionPage.js
+++ app/containers/AdmissionPage.js
@@ -1,19 +1,143 @@
+// TODO: refactro this 140+-line container
+
 import React, { Component } from 'react';
-import Admission from '../components/Admission/Admission'
+import Admission from '../components/Admission/Admission';
+import {
+  Student,
+  Section,
+  StudentFees,
+  sequelize
+} from '../utils/database/index';
+import genericInputHandler from '../utils/misc/genericInputHandler';
+import getLastId from '../utils/misc/getLastID';
+import classOptions from '../constants/classOptions.json';
 
+class AdmissionPage extends Component {
+  constructor(props) {
+    super(props);
+    this.inputHandler = this.inputHandler.bind(this);
+  }
 
-class AdmissionPage extends Component{
-    state = {
+  state = {
+    admissionData: {
+      nextStudentID: 0,
+      name: '',
+      email: '',
+      phoneNumber: '',
+      school: '',
+      address:'',
+      parentName: '',
+      parentOccupation: '',
+      parentPhoneNumber: '',
+      class: '',
+      section: '',
+      payment: '',
+      notes: ''
+    },
+    sections: [],
+    sectionOptions: []
+  };
 
+  inputHandler = genericInputHandler;
+
+  async classInputHandler(e, { name, value }, stateKey) {
+    let foundSections = await Section.findAll({
+      where: {
+        class: value
+      }
+    });
+
+    // mapping the section model returned from the database
+    // into a useful prop for select display
+    let sectionOptions = [];
+    if (foundSections.length > 0) {
+      sectionOptions = foundSections.map((section, index) => ({
+        // ???stateKey???
+        stateKey: section.id,
+        value: section.name,
+        text: section.name
+      }));
     }
 
+    this.setState({
+      sections: foundSections,
+      sectionOptions
+    });
 
-    render() {
-        return (
-            <Admission classes={this.props.classes}/>
-        )
-    }
+    this.inputHandler(e, { name, value }, stateKey);
+  }
+
+  async sectionInputHandler(e, { name, value }, stateKey) {
+    let section = this.state.sections.find(
+      (section, index) => section.name === value
+    );
+    let { defaultAdmissionFees, defaultMonthlyFees, counter } = section;
+    let settledFees =
+      defaultAdmissionFees + ((8 - counter) * defaultMonthlyFees) / 8;
+      
+    // ? removing await breaks it???
+    await this.inputHandler(
+      e,
+      { name: 'payment', value: settledFees },
+      stateKey
+    );
+    this.inputHandler(e, { name, value }, stateKey);
+  }
+
+  // executes only for the initial render
+  async componentDidMount() {
+    // get the last id in the db
+    let lastID = await getLastId(sequelize, 'students');
+    let admissionDataCopy = { ...this.state.admissionData };
+    admissionDataCopy.nextStudentID = lastID + 1;
+    this.setState({ admissionData: admissionDataCopy });
+  }
+
+  async onSubmit(e) {
+    e.preventDefault();
+    let admissionDataCopy = { ...this.state.admissionData };
+    let section = this.state.sections.find(
+      (section, index) => section.name === admissionDataCopy.section
+    );
+
+    let createdStudent = await Student.create({
+      ...admissionDataCopy,
+      sectionId: section.dataValues.id
+    });
+
+    // TODO?: should fees be paid on admission???
+    // TODO?: and if so, is it both monthly and admissions fees?
+    // TODO?: if paid immediatly, add to income
+    let createdStudentFees = await StudentFees.create({
+      studentId: createdStudent.dataValues.id,
+      value: this.state.admissionData.payment
+    });
+
+    // get the last id in the db
+    let lastID = await getLastId(sequelize, 'students');
+    admissionDataCopy.nextStudentID = lastID + 1;
+    this.setState({
+      admissionData: admissionDataCopy
+    });
+  }
+
+  render() {
+    return (
+      <Admission
+        classOptions={classOptions}
+        sectionOptions={this.state.sectionOptions}
+        admissionData={this.state.admissionData}
+        inputHandler={(e, d) => this.inputHandler(e, d, 'admissionData')}
+        classInputHandler={(e, d) =>
+          this.classInputHandler(e, d, 'admissionData')
+        }
+        sectionInputHandler={(e, d) =>
+          this.sectionInputHandler(e, d, 'admissionData')
+        }
+        onSubmit={e => this.onSubmit(e)}
+      />
+    );
+  }
 }
-
 
 export default AdmissionPage;
