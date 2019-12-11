11c203ef9cbc21e13f4b17a1619839e84365dc8e
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2019-12-11 23:32:17 +0200

    - feat: exam evaluation functionality

--- app/components/Exams/EvaluateExam/EvaluateExam.js
+++ app/components/Exams/EvaluateExam/EvaluateExam.js
@@ -1,20 +1,57 @@
-import React from 'react'
+import React from 'react';
 import { Button, Form } from 'semantic-ui-react';
 
-const EvaluateExam = (props)=>(
+const EvaluateExam = props => (
+  <Form onSubmit={props.submitHandler}>
+    <Form.Group unstackable widths={2}>
+      <Form.Input
+        label="Exam Creation Date"
+        type="date"
+        required
+        name="date"
+        value={props.evaluationFormData.examDate}
+        onChange={props.dateInputHandler}
+      />
 
-    <Form>
-      <Form.Group unstackable widths={2}>
-        <Form.Input label='ID' placeholder='1' required />
-        <Form.Input label='Student Marks' placeholder='83' required />
-      </Form.Group>
-      <Button type='submit'>Submit</Button>
-      <ul style = {{listStyleType:"none"}}>
-        <li>This Page Should Only Be Accessed Through Exam Date Layer</li>
-        <li>Take The Exam Date From the date layer's state</li>
-        <li>Create a function to check if the student passed the minimum passing grade</li>
-      </ul>
-    </Form>
-)
+      {/* TODO: exam dropdown */}
+      <Form.Select
+        label="Exam"
+        required
+        name="exam"
+        options={props.examOptions}
+        value={props.evaluationFormData.exam}
+        onChange={props.inputHandler}
+      />
 
-export default EvaluateExam
+      {/* <Form.Input
+        label="Taken on"
+        type="date"
+        required
+        name="takenOn"
+        value={props.evaluationFormData.takenOn}
+        onChange={props.inputHandler}
+      /> */}
+
+      <Form.Input
+        label="ID"
+        placeholder="1"
+        required
+        name="id"
+        value={props.evaluationFormData.id}
+        onChange={props.inputHandler}
+      />
+      <Form.Input
+        label="Score"
+        placeholder="83"
+        type="number"
+        required
+        name="score"
+        value={props.evaluationFormData.score}
+        onChange={props.inputHandler}
+      />
+    </Form.Group>
+    <Button type="submit">Submit</Button>
+  </Form>
+);
+
+export default EvaluateExam;
--- app/containers/EvaluateExamPage.js
+++ app/containers/EvaluateExamPage.js
@@ -1,19 +1,105 @@
 import React, { Component } from 'react';
 import EvaluateExam from '../components/Exams/EvaluateExam/EvaluateExam';
+import genericInputHandler from '../utils/misc/genericInputHandler';
+import { Exam, StudentExam } from '../utils/database/index';
+import { Op } from 'sequelize';
 
+class EvaluateExamPage extends Component {
+  constructor(props) {
+    super(props);
+    this.inputHandler = this.inputHandler.bind(this);
+  }
 
-class EvaluateExamPage extends Component{
-    state = {
-    }
+  state = {
+    evaluationFormData: {
+      // student's id
+      id: '',
+      date: '',
+      // holds exam id
+      exam: '',
+      // takenOn: '',
+      score: 0
+    },
+    exams: [],
+    examOptions: []
+  };
 
-    render() {
-        return (
-          <div>
-            <EvaluateExam/>
-          </div>
-        )
-    }
+  inputHandler = genericInputHandler;
+
+  async dateInputHandler(e, { name, value }, stateKey) {
+    // Setting up a date-time range from the beginning of
+    // the day to its end to use in exam fetching by date
+    let startDate = new Date(value);
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+    startDate = new Date(startDate.getTime() - 1000);
+    let endDate = new Date(value);
+    endDate.setHours(23);
+    endDate.setMinutes(59);
+
+    // fetch exams with the date range (chosen day)
+    let foundExams = await Exam.findAll({
+      where: {
+        date: {
+          [Op.between]: [startDate, endDate]
+        }
+      }
+    });
+
+    // create exam options
+
+    let examOptions = foundExams.map((exam, index) => {
+      return {
+        text: exam.dataValues.name,
+        value: exam.dataValues.id,
+        key: index
+        // other???
+      };
+    });
+
+    // saving stuff to state
+    // put the found exams in state, just in case you need them???
+    // put exam options in state
+    let stateCopy = {
+      ...this.state,
+      evaluationFormData: { ...this.state.evaluationFormData }
+    };
+    stateCopy.examOptions = examOptions;
+    stateCopy.exams = foundExams;
+    stateCopy.evaluationFormData.date = value;
+    await this.setState(stateCopy);
+  }
+
+  async submitHandler(e) {
+    e.preventDefault();
+    let formData = this.state.evaluationFormData;
+    let exam = this.state.exams.find(
+      exam => (exam.dataValues.id = formData.exam)
+    );
+    let createdData = await StudentExam.create({
+      studentId: formData.id,
+      examId: formData.exam,
+      score: formData.score,
+      passing: formData.score >= exam.dataValues.minimum
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <EvaluateExam
+          inputHandler={(e, d) => this.inputHandler(e, d, 'evaluationFormData')}
+          dateInputHandler={(e, d) =>
+            this.dateInputHandler(e, d, 'evaluationFormData')
+          }
+          evaluationFormData={this.state.evaluationFormData}
+          submitHandler={e => this.submitHandler(e)}
+          examOptions={this.state.examOptions}
+        />
+      </div>
+    );
+  }
 }
-
 
 export default EvaluateExamPage;
