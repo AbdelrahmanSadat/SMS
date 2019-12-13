2a27ec00accafb2a7a14a7eb1c1930d4fed00bf6
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2019-12-13 17:09:48 +0200

    - feat: initial attendance functionality

--- app/components/Attendance/Attendance.js
+++ app/components/Attendance/Attendance.js
@@ -3,15 +3,77 @@ import { Form, Input, Button } from 'semantic-ui-react';
 
 const Attendance = props => (
   <div>
-  <Form>
-    <Form.Field inline>
-      <label>ID</label>
-      <Input/>
-    </Form.Field>
-  </Form>
+    <Form>
+      {
+        // TODO: filtering sections by a class input
+        // TODO: required fields not working on first form
+      }
+      <Form.Select
+        label="Class"
+        name="class"
+        placeholder="1st"
+        required
+        name="section"
+        options={props.classOptions}
+        value={props.formData.class}
+        onChange={props.classInputHandler}
+        disabled={props.sessionOn}
+      />
+      <Form.Select
+        label="Section"
+        name="section"
+        placeholder="1"
+        required
+        name="section"
+        options={props.sectionOptions}
+        value={props.formData.section}
+        onChange={props.inputHandler}
+        disabled={props.sessionOn}
+      />
+      <Form.Input
+        label="Increment"
+        placeholder="1"
+        required
+        name="increment"
+        type="number"
+        value={props.formData.increment}
+        onChange={props.inputHandler}
+        disabled={props.sessionOn}
+      />
+      {// TODO: date time validation and maximum value (for db)
+      }
+      <Form.Input
+        label="Start Time"
+        type="datetime-local"
+        required
+        name="date"
+        value={props.formData.date}
+        onChange={props.inputHandler}
+        disabled={props.sessionOn}
+      />
+      {
+        // TODO: alternate buttons according to state
+      }
+      <Button type="submit" onClick={props.startSessionHandler}>
+        Start Session
+      </Button>
+      <Button onClick={props.endSessionHandler}>End Session</Button>
+    </Form>
+    <Form onSubmit={props.idSubmitHandler}>
+      <Form.Input
+        label="ID"
+        placeholder="1"
+        required
+        name="id"
+        value={props.formData.id}
+        onChange={props.inputHandler}
+      />
+      <Button type="submit">Find</Button>
+    </Form>
     <p>Fetch Name From DB</p>
     <p>Fetch Notes From DB</p>
     <ul style={{ listStyleType: 'none' }}>
+      <li>Starting a session</li>
       <li>Automate the attendance process (Focus, Submit, Clear)</li>
       <li>
         Set Section in State & validate student's existance in the section
@@ -19,6 +81,6 @@ const Attendance = props => (
       <li>Integrate with the warningsModal</li>
     </ul>
   </div>
-)
+);
 
-export default Attendance
+export default Attendance;
--- app/containers/AttendancePage.js
+++ app/containers/AttendancePage.js
@@ -1,19 +1,158 @@
 import React, { Component } from 'react';
+import {
+  Section,
+  Student,
+  Attendance as AttendanceModel,
+  StudentWarning,
+  Warning
+} from '../utils/database/index';
 import Attendance from '../components/Attendance/Attendance';
+import classOptions from '../constants/classOptions.json';
+import genericInputHandler from '../utils/misc/genericInputHandler';
 
+class AttendancePage extends Component {
+  constructor(props) {
+    super(props);
+    this.inputHandler = this.inputHandler.bind(this);
+  }
 
-class AttendancePage extends Component{
-    state = {
+  state = {
+    formData: {
+      id: '',
+      section: '',
+      class: '',
+      increment: 1,
+      date: ''
+    },
+    classOptions,
+    sections: [],
+    sectionOptions: [],
+    sessionOn: false,
+    openSection: {}
+  };
+
+  inputHandler = genericInputHandler;
+
+  async classInputHandler(e, { name, value }, stateKey) {
+    let foundSections = await Section.findAll({
+      where: {
+        class: value
+      }
+    });
+
+    // mapping the section model returned from the database
+    // into a useful prop for select display
+    let sectionOptions = [];
+    if (foundSections.length > 0) {
+      sectionOptions = foundSections.map((section, index) => ({
+        value: section.id,
+        text: section.name
+      }));
     }
 
-    render() {
-        return (
-          <div>
-            <Attendance/>
-          </div>
-        )
+    let stateCopy = { ...this.state, formData: { ...this.state.formData } };
+    stateCopy.formData.class = value;
+
+    this.setState({
+      ...stateCopy,
+      sections: foundSections,
+      sectionOptions
+    });
+  }
+
+  async startSessionHandler(e) {
+    // e.preventDefault();
+    let openSection = this.state.sections.find(
+      section => section.id === this.state.formData.section
+    );
+    await this.setState({
+      openSection: openSection,
+      sessionOn: true
+    });
+    console.log(this.state);
+    // increment section counter
+    openSection.counter += this.state.formData.increment;
+    openSection.save()
+    // TODO: handler counter reaching max, auto add fees
+    //? could be handled from DB models
+    // TODO: find all students assigned to section
+    let assignedStudents = await Student.findAll({
+      where: {
+        sectionId: openSection.id
+      }
+    });
+    console.log(assignedStudents);
+    let attendanceBulkCreateArray = assignedStudents.map((student, index) => {
+      return {
+        studentId: student.id,
+        sectionId: openSection.id,
+        date: this.state.formData.date
+      };
+    });
+    let createdAttendances = await AttendanceModel.bulkCreate(
+      attendanceBulkCreateArray
+    );
+  }
+
+  endSessionHandler(e) {
+    this.setState({
+      sessionOn: false,
+      openSection: {}
+    });
+  }
+
+  async idSubmitHandler(e) {
+    e.preventDefault();
+    // TODO?: can keep all the created attendance records (in state?) and
+    // ? update them as needed whenever a student attends
+    // ? instead of fetching a student and searching attendance every time
+    // take the id, find a student who has it
+    let foundStudent = await Student.findOne({
+      where: { id: this.state.formData.id },
+      include: [Warning]
+    });
+    console.log(foundStudent)
+    // check if the open section is the same as that student's assigned section
+    if (foundStudent.sectionId != this.state.openSection.id) {
+      // TODO: warning
+      console.log('WRONG SECTION!');
     }
+    // check if there are any warnings on the student
+    if (foundStudent.warnings.length > 0) {
+      console.log("WARNINGS")
+      // TODO: warnings
+      // if any exist, display the warnings in text for now
+      console.log(foundStudent.warnings)
+    }
+    // change to attended
+    let attendanceRecord = await AttendanceModel.findOne({
+      where: {
+        studentId: foundStudent.id,
+        sectionId: this.state.openSection.id,
+        date: this.state.formData.date
+      }
+    })
+    attendanceRecord.attended = true;
+    attendanceRecord.save().then(()=>console.log("updated record"))
+  }
+
+  render() {
+    return (
+      <div>
+        <Attendance
+          inputHandler={(e, d) => this.inputHandler(e, d, 'formData')}
+          classInputHandler={(e, d) => this.classInputHandler(e, d, 'formData')}
+          startSessionHandler={e => this.startSessionHandler(e)}
+          endSessionHandler={e => this.endSessionHandler(e)}
+          idSubmitHandler={e => this.idSubmitHandler(e)}
+          formData={this.state.formData}
+          classOptions={this.state.classOptions}
+          sectionOptions={this.state.sectionOptions}
+          sessionOn={this.state.sessionOn}
+        />
+      </div>
+    );
+  }
 }
-
 
 export default AttendancePage;
