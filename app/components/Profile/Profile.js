a1eb4be47853669d66151273c3f4c3c4262c5b38
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2020-01-29 17:36:28 +0200

    - feat: initial implementation of student profile - Basic student data is editable - Not all fields are editable yet - Like all other components at this point, it   requires testing, bug fixes, validation, and   error handling

--- app/components/Profile/Profile.js
+++ app/components/Profile/Profile.js
@@ -1,13 +1,140 @@
 import React from 'react';
+import { Form, Input, Button } from 'semantic-ui-react';
 
 const Profile = props => (
   <div>
+    {/* The first form is for searching for a student */}
+    <Form onSubmit={props.studentSearchOnSubmit}>
+      <Form.Input
+        label="ID"
+        placeholder="1"
+        required
+        name="id"
+        value={props.studentSearchFormData.id}
+        onChange={props.studentSearchInputHandler}
+      />
+      <Button type="submit">Find</Button>
+    </Form>
+
+    {/* The second form is to edit the student data */}
+    <Form onSubmit={props.studentEditOnSubmit}>
+      {/* // TODO: the inputs can be refactored to be generated instead
+          of hard-coded */}
+      <Form.Checkbox
+        name="allowEditing"
+        label="Enable Editing"
+        checked={props.studentEditFormData.allowEditing}
+        onChange={props.studentEditInputHandler}
+      />
+      <Form.Input
+        name="address"
+        label="Address"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.address}
+      />
+      <Form.Input
+        name="class"
+        label="Class"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.class}
+      />
+      <Form.Input
+        name="email"
+        label="Email"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.email}
+      />
+      <Form.Input
+        name="id"
+        label="ID"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.id}
+      />
+      <Form.Input
+        name="name"
+        label="Name"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.name}
+      />
+      <Form.Input
+        name="notes"
+        label="notes"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.notes}
+      />
+      <Form.Input
+        name="parentName"
+        label="Parent's Name"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.parentName}
+      />
+      <Form.Input
+        name="parentOccupation"
+        label="Parent's Occupation"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.parentOccupation}
+      />
+      <Form.Input
+        name="parentPhoneNumber"
+        label="Parent's Phone Number"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.parentPhoneNumber}
+      />
+      <Form.Input
+        name="phoneNumber"
+        label="Phone Number"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.phoneNumber}
+      />
+      {/* // TODO: properly implement reservation date editing */}
+      {/* <Form.Input
+        name="reservationDate"
+        label="Reservation Date"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.reservationDate}
+      /> */}
+      <Form.Input
+        name="School"
+        label="School"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.school}
+      />
+      {/* // TODO: Allow section editing */}
+      {/* <Form.Input name="section" onChange={props.studentEditInputHandler}
+      disabled={!props.studentEditFormData.allowEditing}/> */}
+      <Form.Input
+        name="status"
+        label="Status"
+        onChange={props.studentEditInputHandler}
+        disabled={!props.studentEditFormData.allowEditing}
+        value={props.studentEditFormData.status}
+      />
+      <Button type="submit" disabled={!props.studentEditFormData.allowEditing}>
+        Submit
+      </Button>
+    </Form>
+
     <ul style={{ listStyleType: 'none' }}>
       <li>Display Student Data</li>
-      <li>Create a select input with the three status: Active, Wanted(Prompts Reason), Terminated(Prompts Reason)</li>
+      <li>
+        Create a select input with the three status: Active, Wanted(Prompts
+        Reason), Terminated(Prompts Reason)
+      </li>
       <li>Everything is editable except for the ID & Date of registeration</li>
     </ul>
   </div>
-)
+);
 
-export default Profile
+export default Profile;
--- app/containers/ProfilePage.js
+++ app/containers/ProfilePage.js
@@ -1,19 +1,102 @@
 import React, { Component } from 'react';
+import { cloneDeep, merge } from 'lodash';
 import Profile from '../components/Profile/Profile';
+import genericInputHandler from '../utils/misc/genericInputHandler';
+import { Student, Section } from '../utils/database/index';
 
+class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
+    this.inputHandler = this.inputHandler.bind(this);
+  }
 
-class ProfilePage extends Component{
-    state = {
-    }
+  state = {
+    studentSearchFormData: {
+      id: ''
+    },
+    // * contains the student data, which can be edited
+    studentEditFormData: {
+      id: '',
+      name: '',
+      email: '',
+      phoneNumber: '',
+      parentName: '',
+      parentPhoneNumber: '',
+      parentOccupation: '',
+      address: '',
+      notes: '',
+      school: '',
+      class: '',
+      status: '',
+      attendanceCounter: '', // ? not sure how this can be used here
+      section: {},
+      warnings: [],
+      attendance: [], // ? not sure to display this here or not
+      // * controlled by a checkbox, enables/disables editing
+      // * the currently fetched student
+      allowEditing: false
+    },
+    student: {}
+  };
 
-    render() {
-        return (
-          <div>
-            <Profile/>
-          </div>
-        )
-    }
+  inputHandler = genericInputHandler;
+
+  // * Finds a student to display their data and allow its editing
+  async studentSearchOnSubmit(e) {
+    e.preventDefault();
+
+    let foundStudent = await Student.findOne({
+      where: { id: this.state.studentSearchFormData.id },
+      include: [{ model: Section }]
+    });
+
+    this.setState({
+      student: foundStudent
+    });
+
+    this.setState({
+      studentEditFormData: { ...foundStudent.dataValues }
+    });
+
+    console.log(foundStudent.dataValues);
+  }
+
+  // * Submits the newly edited student data
+  async studentEditOnSubmit(e) {
+    e.preventDefault();
+
+    // TODO:
+    // * Update the student model with the new data from the form
+    // * Note that some of the student model's fields are not
+    // * in the form data
+    let studentCopy = cloneDeep(this.state.student);
+
+    // merges the new values with the student model
+    merge(studentCopy, studentCopy, this.state.studentEditFormData);
+    console.log(studentCopy);
+    let savedStudent = await studentCopy.save();
+
+    this.setState({ student: savedStudent });
+  }
+
+  render() {
+    return (
+      <div>
+        <Profile
+          studentSearchFormData={this.state.studentSearchFormData}
+          studentSearchInputHandler={(e, d) =>
+            this.inputHandler(e, d, 'studentSearchFormData')
+          }
+          studentSearchOnSubmit={e => this.studentSearchOnSubmit(e)}
+          studentEditFormData={this.state.studentEditFormData}
+          studentEditInputHandler={(e, d) =>
+            this.inputHandler(e, d, 'studentEditFormData')
+          }
+          studentEditOnSubmit={e => this.studentEditOnSubmit(e)}
+        />
+      </div>
+    );
+  }
 }
-
 
 export default ProfilePage;
