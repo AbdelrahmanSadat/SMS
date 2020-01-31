8b070d482d58c009cc8b2b5626bfe97a4e9dcd3c
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2020-01-31 02:00:11 +0200

    - chore?: Initial (non-functional) implementation   of a generic AddFees component - Should display one of the add fees components   according to the user's choice - Come to think about it, it sounds like a terrible   idea when you can just use navigation and routing   to access these components from a higher level - Just a lazy commit to clear my local repo

--- app/components/Payment/AddFees/AddFees.js
+++ app/components/Payment/AddFees/AddFees.js
@@ -1,22 +1,43 @@
+// ! Currently not being used
+
+// * Renders one of the AddFees componenets according to 
+// * user's selection
+
 import React from 'react';
 import { Button, Form } from 'semantic-ui-react';
 
 const AddFees = props => (
-  <Form>
-    <Form.Group unstackable widths={2}>
-      <Form.Select
-        required
-        fluid
-        label='Type'
-        // options={props.classes}
-        //this select menu should hold options of student/class/section/session each prompting a specific ID/Name/Date
-        //through the adjacent Input Field
-        placeholder='Student'
+    <Form>
+      <Form.Group unstackable widths={2}>
+        <Form.Select
+          required
+          fluid
+          name="type"
+          label="Type"
+          // options={props.classes}
+          //this select menu should hold options of student/class/section/session each prompting a specific ID/Name/Date
+          //through the adjacent Input Field
+          options={props.addFeesTypeOptions}
+          placeholder="Student"
+          value={props.formData.type}
+          onChange={props.inputHandler}
         />
-        <Form.Input label='ID' placeholder='1' required />
-    </Form.Group>
-    <Button type='submit'>Submit</Button>
-  </Form>
-)
+        {
+          // TODO: this renders conditionally according to
+          // TODO: the value of the last input
+        }
+        {/* <Form.Input
+        name="id"
+        label="ID"
+        placeholder="1"
+        required
+        value={props.formData.identifier}
+        onChange={props.inputHandler}
+      /> */}
+      </Form.Group>
+    <Button type="submit">Submit</Button>
+    {props.identifierElement}
+    </Form>
+);
 
-export default AddFees
+export default AddFees;
--- app/containers/AddFeesPage.js
+++ app/containers/AddFeesPage.js
@@ -1,19 +1,102 @@
+// ! Currently not being used
+
+// * Renders one of the AddFees componenets according to 
+// * user's selection
+
 import React, { Component } from 'react';
+import addFeesTypeOptions from '../constants/addFeesTypeOptions.json';
+import classOptions from '../constants/classOptions.json';
 import AddFees from '../components/Payment/AddFees/AddFees';
+import genericInputHandler from '../utils/misc/genericInputHandler';
+import { Button, Form } from 'semantic-ui-react';
 
 
-class AddFeesPage extends Component{
-    state = {
+class AddFeesPage extends Component {
+  constructor(props) {
+    super(props);
+    this.inputHandler = this.inputHandler.bind(this);
+  }
+
+  state = {
+    formData: {
+      type: '',
+      identifier: ''
+    },
+    addFeesTypeOptions,
+    classOptions,
+    // sections fetched from the database?
+    sections: []
+  };
+
+  inputHandler = genericInputHandler;
+
+  render() {
+    let identifierElement;
+    switch (this.state.formData.type) {
+      case 'section':
+        <p>Section :|</p>
+        // <Form.Select
+        //   required
+        //   fluid
+        //   label="Type"
+        //   options={
+        //     this.state.classOptions //through the adjacent Input Field //this select menu should hold options of student/class/section/session each prompting a specific ID/Name/Date // options={props.classes}
+        //   }
+        //   placeholder="Class"
+        //   value={props.formData.identifier}
+        //   onChange={(e, d) => this.state.inputHandler(e, d, 'formData')}
+        //   name="identifier"
+        // />;
+        break;
+      case 'student':
+        identifierElement = identifierElement = (
+          <Form.Input
+            required
+            fluid
+            label="ID"
+            placeholder="ID"
+            value={this.state.formData.identifier}
+            onChange={(e, d) => this.inputHandler(e, d, 'formData')}
+            name="identifier"
+          />
+        );
+        break;
+      case 'class':
+        identifierElement = identifierElement = (
+          <Form.Select
+            required
+            fluid
+            label="Type"
+            // through the adjacent Input Field 
+            // this select menu should hold options 
+            // bof student /class/section/session 
+            // each prompting a specific ID / Name / Date 
+            // options={props.classes}
+            options={this.state.classOptions}
+            placeholder="Class"
+            value={this.state.formData.identifier}
+            onChange={(e, d) => this.inputHandler(e, d, 'formData')}
+            name="identifier"
+          />
+        );
+        break;
+      case 'session':
+        identifierElement = <p style={{ color: 'black' }}>Session</p>;
+        break;
+      default:
     }
 
-    render() {
-        return (
-          <div>
-            <AddFees/>
-          </div>
-        )
-    }
+    return (
+      <div>
+        <AddFees
+          formData={this.state.formData}
+          addFeesTypeOptions={this.state.addFeesTypeOptions}
+          identifierElement={identifierElement}
+          inputHandler={(e, d) => this.inputHandler(e, d, 'formData')}
+        />
+      </div>
+    );
+  }
 }
-
 
 export default AddFeesPage;
