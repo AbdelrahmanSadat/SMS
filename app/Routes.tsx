fdf8a5651f17871c4df0976fcb76bd91c4d2a8bf
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2020-06-29 23:33:36 +0200

    - feat: Added basic layout w/ routing

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
-// import HomePage from './containers/HomePage';
 
 //* The boilerplate uses a container for each component?
 //* and imports that container which in turn renders
 //* component
 
+import HomePage from './containers/HomePage';
 import AdmissionPage from './containers/AdmissionPage';
 import AttendancePage from './containers/AttendancePage';
 // import WarningPage from './containers/WarningPage';
@@ -25,27 +25,24 @@ import AddSectionPage from './containers/AddSectionPage';
 import StartSessionPage from './containers/StartSessionPage';
 import CreateExamPage from './containers/CreateExamPage';
 import EvaluateExamPage from './containers/EvaluateExamPage';
-import HomePage from './containers/HomePage';
 // import DateLayerPage from './containers/DateLayerPage';
-// import Layout from './containers/Layout';
 
+// TODO: Remove this boilerplate code
 // Lazily load routes and code split with webpacck
-const LazyCounterPage = React.lazy(() =>
-  import(/* webpackChunkName: "CounterPage" */ './containers/CounterPage')
-);
+// const LazyCounterPage = React.lazy(() =>
+//   import(/* webpackChunkName: "CounterPage" */ './containers/CounterPage')
+// );
 
-const CounterPage = (props: Record<string, any>) => (
-  <React.Suspense fallback={<h1>Loading...</h1>}>
-    <LazyCounterPage {...props} />
-  </React.Suspense>
-);
+// const CounterPage = (props: Record<string, any>) => (
+//   <React.Suspense fallback={<h1>Loading...</h1>}>
+//     <LazyCounterPage {...props} />
+//   </React.Suspense>
+// );
 
 export default function Routes() {
   return (
     <App>
       <Switch>
-        {/* <Route path={routes.HOME} component={Layout} /> */}
-        <Route path={routes.HOME} component={HomePage} />
         {/* <Route path={routes.HR} component={} /> */}
         {/* <Route path={routes.REPORTS} component={} /> */}
         {/* <Route path={routes.SEMESTER} component={} /> */}
@@ -53,6 +50,7 @@ export default function Routes() {
         {/* <Route path={routes.INCOME} component={} /> */}
         {/* <Route path={routes.LOGIN} component={} /> */}
 
+        {/* <Route path={routes.HOME} component={HomePage} /> */}
         <Route path={routes.ADDFEESTOCLASS} component={AddFeesToClassPage} />
         <Route
           path={routes.ADDFEESTOSECTION}
@@ -81,6 +79,8 @@ export default function Routes() {
         <Route path={routes.ADDSECTION} component={AddSectionPage} />
 
         <Route path={routes.STARTSESSION} component={StartSessionPage} />
+        <Route exact path={routes.HOME} component={HomePage} />
+        {/* <Route path={routes.CATCHALL} component={Layout} /> */}
       </Switch>
     </App>
   );
--- app/containers/Layout.tsx
+++ app/containers/Layout.tsx
@@ -1,62 +1,138 @@
-import { useBooleanKnob } from '@stardust-ui/docs-components'
-import React from 'react'
-import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Button,
+  // Dropdown,
+  Icon,
+  Menu,
+  Segment,
+  Sidebar
+} from 'semantic-ui-react';
+import routes from '../constants/routes.json';
 
-const SidebarExampleMultiple = () => {
-  const [visible, setVisible] = useBooleanKnob({ name: 'visible' })
+// TODO: Make the sidebar's height constant across pages
+// TODO: Make it look nicer, remove unnecessary spacing at the top
+// TODO: Figure out why there are random line separations in the menu
 
-  return (
-    <Sidebar.Pushable as={Segment}>
-      <Sidebar
-        as={Menu}
-        animation='overlay'
-        direction='left'
-        icon='labeled'
-        inverted
-        onHide={() => setVisible(false)}
-        vertical
-        visible={visible}
-        width='thin'
-      >
-        <Menu.Item as='a'>
-          <Icon name='home' />
-          Home
-        </Menu.Item>
-        <Menu.Item as='a'>
-          <Icon name='gamepad' />
-          Games
-        </Menu.Item>
-        <Menu.Item as='a'>
-          <Icon name='camera' />
-          Channels
-        </Menu.Item>
-      </Sidebar>
+class Layout extends Component {
+  state = {
+    sidebarVisibility: false
+  };
+  setSidebarVisibility(visibility: Boolean) {
+    this.setState({ sidebarVisibility: visibility });
+  }
+  render() {
+    console.log("THIS IS THE ROUTE:");
+    console.log(routes.PAYMENT);
+    return (
+      // <Sidebar.Pushable as={Segment}>
+      <Sidebar.Pushable>
+        <Menu borderless>
+          <Menu.Item fitted>
+            <Button
+              icon
+              compact
+              onClick={_e => this.setSidebarVisibility(true)}
+            >
+              <Icon name="bars" />
+            </Button>
+          </Menu.Item>
+        </Menu>
+        <Sidebar
+          as={Menu}
+          // icon="labeled"
+          animation="push"
+          direction="left"
+          inverted
+          onHide={() => this.setSidebarVisibility(false)}
+          vertical
+          visible={this.state.sidebarVisibility}
+          width="thin"
+          compact
+        >
+          <Menu.Item name="home">
+            <Link to={routes.HOME}>Home</Link>
+          </Menu.Item>
+          <Menu.Item name="payments">
+            Payments
+            <Menu.Menu>
+              <Menu.Item name="payment">
+                <Link to={routes.PAYMENT}>Payment</Link>
+              </Menu.Item>
+              <Menu.Item name="paymentGroup">
+                <Link to={routes.ADDPAYMENTGROUP}>Create Payment Group</Link>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu.Item>
+          <Menu.Item name="addFees">
+            Add Fees
+            <Menu.Menu>
+              <Menu.Item name="addFeesToClass">
+                <Link to={routes.ADDFEESTOCLASS}>Class</Link>
+              </Menu.Item>
+              <Menu.Item name="addFeesToStudent">
+                <Link to={routes.ADDFEESTOSTUDENT}>Student</Link>
+              </Menu.Item>
+              <Menu.Item name="addFeesToSection">
+                <Link to={routes.ADDFEESTOSECTION}>Section</Link>
+              </Menu.Item>
+              <Menu.Item name="addFeesToSession">
+                <Link to={routes.ADDFEESTOSESSION}>Session</Link>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu.Item>
+          <Menu.Item name="attendance">
+            <Link to={routes.ATTENDANCE}>Attendance</Link>
+          </Menu.Item>
+          <Menu.Item name="admission">
+            <Link to={routes.ADMISSION}>Admission</Link>
+          </Menu.Item>
+          <Menu.Item name="exams">
+            Exams
+            <Menu.Menu>
+              <Menu.Item name="createExam">
+                <Link to={routes.CREATEEXAM}>Create</Link>
+              </Menu.Item>
+              <Menu.Item name="evaluateExam">
+                <Link to={routes.EVALUATEEXAM}>Evaluate</Link>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu.Item>
+          <Menu.Item name="students">
+            Students
+            <Menu.Menu>
+              <Menu.Item name="studentProfile">
+                <Link to={routes.PROFILE}>Profile</Link>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu.Item>
+          <Menu.Item name="admission">
+            <Link to={routes.ADMISSION}>Admission</Link>
+          </Menu.Item>
+          <Menu.Item name="sections">
+            Sections
+            <Menu.Menu>
+              <Menu.Item name="addSection">
+                <Link to={routes.ADDSECTION}>Add Section</Link>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu.Item>
+          <Menu.Item name="session">
+            Sections
+            <Menu.Menu>
+              <Menu.Item name="startSession">
+                <Link to={routes.STARTSESSION}>Start Session</Link>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu.Item>
+        </Sidebar>
 
-      <Sidebar
-        as={Menu}
-        animation='overlay'
-        direction='right'
-        inverted
-        vertical
-        visible={visible}
-      >
-        <Menu.Item as='a' header>
-          File Permissions
-        </Menu.Item>
-        <Menu.Item as='a'>Share on Social</Menu.Item>
-        <Menu.Item as='a'>Share by E-mail</Menu.Item>
-        <Menu.Item as='a'>Edit Permissions</Menu.Item>
-        <Menu.Item as='a'>Delete Permanently</Menu.Item>
-      </Sidebar>
-
-      <Sidebar.Pusher>
-        <Segment basic>
-          <Header as='h3'>Application Content</Header>
-          <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-        </Segment>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
-  )
+        <Sidebar.Pusher>
+          <Segment basic>{this.props.children}</Segment>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    );
+  }
 }
 
-export default SidebarExampleMultiple
+export default Layout;
